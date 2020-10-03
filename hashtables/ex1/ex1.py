d40031879cb2f8b49f5c132ce893ae7b2c8f90eb
Stephanie <user.name>
2020-10-02 17:05:34 -0700

    weights.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,23 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    dictionary = {}
 
-    return None
+    for i in range(len(weights)):
+        second_Number = limit - weights[i]
+        if(second_Number in dictionary.keys()):
+            secondIndex = weights.index(second_Number)
+            if(i != secondIndex):
+                return sorted([i, secondIndex])
+        else:
+            dictionary.update({weights[i]: i})
+
+
+# instantiate an empty dictionary
+# for each number in list of numbers:
+#     result = subtract number from target number
+#     look for result in the dictionary (instant lookup)
+#     if found:
+#         return index of number and index of dictionary lookup result
+#     else:
+#         add number to dictionary as key with value being the index
