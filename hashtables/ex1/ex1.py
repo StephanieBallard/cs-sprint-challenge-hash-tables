f269cce38ec3a753b9fd4e0bf74aafb2b7c812d6
Stephanie <user.name>
2020-10-02 17:14:36 -0700

    changed answer, still only passing 1 test

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -6,13 +6,16 @@ def get_indices_of_item_weights(weights, length, limit):
     dictionary = {}
 
     for i in range(len(weights)):
-        second_Number = limit - weights[i]
-        if(second_Number in dictionary.keys()):
-            secondIndex = weights.index(second_Number)
-            if(i != secondIndex):
-                return sorted([i, secondIndex])
-        else:
-            dictionary.update({weights[i]: i})
+        weight = weights[i]
+        pair = limit - weight
+        if pair in dictionary:
+            return [dictionary[pair], i]
+        dictionary[weight] = i
+    return None
+
+
+        
+
 
 
 # instantiate an empty dictionary
@@ -23,3 +26,11 @@ def get_indices_of_item_weights(weights, length, limit):
 #         return index of number and index of dictionary lookup result
 #     else:
 #         add number to dictionary as key with value being the index
+
+# second_Number = limit - weights[i]
+#         if(second_Number in dictionary.keys()):
+#             secondIndex = weights.index(second_Number)
+#             if(i != secondIndex):
+#                 return sorted([i, secondIndex])
+#         else:
+#             dictionary.update({weights[i]: i})
